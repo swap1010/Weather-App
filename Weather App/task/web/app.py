22c4dae5831dd0b2554b0f245f25420d81b13ffe
Swapnil Goyal <57146330+swap1010@users.noreply.github.com>
2021-05-24 20:18:01 +0530

    Add files via upload

---
+++ Weather App/task/web/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import sys
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hello, world!'
+
+
+# don't change the following way to run flask:
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        arg_host, arg_port = sys.argv[1].split(':')
+        app.run(host=arg_host, port=arg_port)
+    else:
+        app.run()
